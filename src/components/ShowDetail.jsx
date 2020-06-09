8c7a5a4c1a4194ee7a5d30146ab7885b2f7ea9de
MarigonaN <marigonanuhija@gmail.com>
2020-06-09 15:58:49 +0200

    Create ShowDetail.jsx

---
+++ src/components/ShowDetail.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Row } from "react-bootstrap";
+
+
+
+const ShowDetail = ({ title }) => (
+    <>
+      <h4>{title}</h4>
+      <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 text-center">
+       
+      </Row>
+    </>
+  );
+
+
+
+
+
+
+
+
+
+
+export default ShowDetail;
