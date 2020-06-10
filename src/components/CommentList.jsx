1e889db4fcc2ad751fa43c48a68e2c8c40834a46
MarigonaN <marigonanuhija@gmail.com>
2020-06-10 23:58:24 +0200

    Update CommentList.jsx

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,52 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
 import { Badge, ListGroup } from "react-bootstrap";
+import { render } from "@testing-library/react";
 
-class CommentList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movieID: this.props.movieId,
-      comments: [],
-    };
+class CommentList extends React.Component{
+  state = {
+    comments:[]
   }
-  componentDidMount = () => {
-    this.fetchComments();
-  };
 
-  fetchComments = async () => {
-    const commentsUrl = "https://striveschool.herokuapp.com/api/comments/";
-    const comments = await fetch(commentsUrl + this.state.movieID, {
-      headers: new Headers({
-        Authorization: "Basic " + btoa("user19:Hxx8R4wZfCANamrj"),
-      }),
-    }).then((response) => response.json());
-    this.setState({ comments: comments });
-  };
 
-  componentWillUnmount = () => {
-    console.log("bye bye");
-  };
-  render() {
-    if (this.props.callFetch) {
-      this.fetchComments();
-    }
-    return (
-      <>
-        {this.state.comments.map((comment) => {
-          return (
-            <ListGroup key={comment._id}>
-              <ListGroup.Item>
-                <Badge pill variant="info" className="mr-3">
-                  {comment.rate}
-                </Badge>
-                {comment.comment}
-              </ListGroup.Item>
-            </ListGroup>
-          );
-        })}
-      </>
-    );
-  }
+  componentDidMount = async () => {
+      const username = 'user19';
+      const password = 'dXNlcjE5Okh4eDhSNHdaZkNBTmFtcmo6';
+
+      const headers = new Headers();
+
+      headers.append('Content-Type', 'application/json');
+      headers.append('Authorization', 'Basic ' + btoa(username + ":" + password));
+
+        const commentsUrl = "https://striveschool.herokuapp.com/api/comments/";
+        const comments = await fetch(commentsUrl + this.props.id, {
+          headers: headers,
+        }).then((response) => response.json());
+        this.setState({ comments });
+      
+}
+
+
+render (){
+  return(
+    this.state.comments.map((comment)=>(
+      <ListGroup key={comment._id}>
+       <ListGroup.Item>
+         <Badge pill variant="info" className="mr-3">
+           {comment.rate}
+         </Badge>
+         {comment.comment}
+       </ListGroup.Item>
+     </ListGroup>
+    ))
+  )
+}
+
 }
 
 export default CommentList;
