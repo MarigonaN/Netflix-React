51d0e8ad047b1d1e8c2629a769bf4c6598b8dfe9
MarigonaN <marigonanuhija@gmail.com>
2020-06-10 23:11:35 +0200

    Create Register.jsx

---
+++ src/components/Register.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const Register = ({ title }) => (
+    <>
+      <h4>{title}</h4>
+      <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 text-center">
+       
+      </Row>
+    </>
+  );
+
+
+export default Register;
